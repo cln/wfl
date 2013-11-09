bf1dc1d7ff9c9753014e4c1b954556fc01e1a4f1
cln <cln.github@gmail.com>
2013-11-09 19:51:59 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wfl::Application.config.secret_token = '5f7795faa2f8e16b2a14846726bf0328e78118ef331fec9e1b4f9c1b062ac37610f66d3b91dcf39b2a096240db34ad36934bf7b0ea268d2e4ab27c7d852a7680'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Wfl::Application.config.session_store :cookie_store, key: '_wfl_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Wfl::Application.config.session_store :active_record_store
